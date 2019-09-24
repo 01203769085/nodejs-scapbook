eb902f5aa3562cec5c698939b6cf20f1b81c7f20
Duc Thuan <dotthan123@gmail.com>
2019-09-24 19:11:16 +0700

    ghi chu post

--- routes/routeBlogs.js
+++ routes/routeBlogs.js
@@ -26,32 +26,32 @@ router.get("/blogsbypage", async (req, res) => {
   let response = await modelBlogs.getBlogsByPage(page, limit);
   res.send(response);
 });
-/******************************************************************** */
-router.post("/", async (req, res) => {
-  let defaultImgPath =
-    "https://res.cloudinary.com/dotthan123/image/upload/v1568818993/sample.jpg";
-  let imgPath = defaultImgPath;
-  if (req.files !== null && typeof req.files !== "undefined") {
-    let file = req.files.fileImage;
-    imgPath = await new Promise(function(resolve, reject) {
-      cloudinary.v2.uploader
-        .upload_stream(options, (err, result) => {
-          if (err) {
-            console.log(err.message);
-            reject(defaultImgPath);
-          } else {
-            resolve(result.url);
-          }
-        })
-        .end(file.data);
-    });
-  }
-  let { title, description, timeShot } = req.body;
-  let arr = timeShot.split("-");
-  timeShot = arr[2] + "-" + arr[1] + "-" + arr[0];
-  await modelBlogs.createBlog(title, description, imgPath, timeShot);
-  res.sendStatus(200);
-});
-/**************************************************************************/
+
+// router.post("/", async (req, res) => {
+//   let defaultImgPath =
+//     "https://res.cloudinary.com/dotthan123/image/upload/v1568818993/sample.jpg";
+//   let imgPath = defaultImgPath;
+//   if (req.files !== null && typeof req.files !== "undefined") {
+//     let file = req.files.fileImage;
+//     imgPath = await new Promise(function(resolve, reject) {
+//       cloudinary.v2.uploader
+//         .upload_stream(options, (err, result) => {
+//           if (err) {
+//             console.log(err.message);
+//             reject(defaultImgPath);
+//           } else {
+//             resolve(result.url);
+//           }
+//         })
+//         .end(file.data);
+//     });
+//   }
+//   let { title, description, timeShot } = req.body;
+//   let arr = timeShot.split("-");
+//   timeShot = arr[2] + "-" + arr[1] + "-" + arr[0];
+//   await modelBlogs.createBlog(title, description, imgPath, timeShot);
+//   res.sendStatus(200);
+// });
+
 
 module.exports = router;
